af0e01514c8d8fc0bd704c58562a265672394b86
[navmich] <[navmich01@gmail.com]>
2020-08-06 13:10:20 +0200

    propojeni s HEROKU (nasazeny backend) - nahrazeni TODO_JPA_API_URL const

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -1,2 +1,4 @@
 export const API_URL = 'http://localhost:8080';
 export const TODO_JPA_API_URL = 'http://localhost:8080/jpa';
+export const TODO_JPA_HEROKU =
+  'https://todo-full-stack-app-backend.herokuapp.com/jpa';
--- src/app/service/data/todo-data.service.ts
+++ src/app/service/data/todo-data.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, Injector } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Todo } from 'src/app/clases/Todo';
-import { TODO_JPA_API_URL } from 'src/app/app.constants';
+import { TODO_JPA_API_URL, TODO_JPA_HEROKU } from 'src/app/app.constants';
 
 @Injectable({
   providedIn: 'root',
@@ -10,31 +10,29 @@ export class TodoDataService {
   constructor(private http: HttpClient, injector: Injector) {}
 
   retrieveAllTodos(username: String) {
-    return this.http.get<Todo[]>(`${TODO_JPA_API_URL}/users/${username}/todos`);
+    return this.http.get<Todo[]>(`${TODO_JPA_HEROKU}/users/${username}/todos`);
   }
 
   deleteTodoById(username: string, id: number) {
-    return this.http.delete(
-      `${TODO_JPA_API_URL}/users/${username}/todos/${id}`
-    );
+    return this.http.delete(`${TODO_JPA_HEROKU}/users/${username}/todos/${id}`);
   }
 
   retrieveTodoById(username: string, id: number) {
     return this.http.get<Todo>(
-      `${TODO_JPA_API_URL}/users/${username}/todos/${id}`
+      `${TODO_JPA_HEROKU}/users/${username}/todos/${id}`
     );
   }
 
   updateTodoById(username: string, id: number, todo: Todo) {
     return this.http.put<Todo>(
-      `${TODO_JPA_API_URL}/users/${username}/todos/${id}`,
+      `${TODO_JPA_HEROKU}/users/${username}/todos/${id}`,
       todo
     );
   }
 
   createTodo(username: string, todo: Todo) {
     return this.http.post<Todo>(
-      `${TODO_JPA_API_URL}/users/${username}/todos`,
+      `${TODO_JPA_HEROKU}/users/${username}/todos`,
       todo
     );
   }
